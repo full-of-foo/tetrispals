49854d4eea87fb4481721e799c776b8012728415
Anthony Troy <lightweightdevelopment@gmail.com>
2013-02-08 18:14:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tetrispals::Application.config.secret_token = '9c5898b6f3ff287a6e456262584b7af89e17997f2f8c9f6d2db0224d579e6b49aa21106898f242fc86d0ce7c943a544491f84b582a71b767640784e93f677a61'
