d0837796389fc6af5eee66dcf80cc14cf2ca9ab6
Anthony Troy <lightweightdevelopment@gmail.com>
2013-03-06 21:29:12 +0000

    Impl leaderboard ajax pagination

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,4 +29,10 @@ $(function() {
         timer = setTimeout($.get($("#users_search").attr("action"), $("#users_search").serialize(), null, "script"), 1000);
         return false;
   });
+
+	$("#high-scores .pagination a").live("click", function () {
+		$("#high-scores .pagination").html('<div class="progress progress-info progress-striped active"><div class="bar" style="width: 80%"></div></div>');
+		$.getScript(this.href);
+		return false;
+	});
 });
